a4f1d7c21b3609298383a9881e5b7e23aecbecc1
NadezhdaAntanachuk <140953233+NadezhdaAntanachuk@users.noreply.github.com>
2024-03-31 17:05:12 +0300

    project updated 001

---
+++ src/main/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,24 @@
+package ru.netology.selenide;
+
+public class AppCardDeliveryTaskOneTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $(cssSelector("[data-test-id='city'] input")).setValue("Тверь");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $(cssSelector("[data-test-id='date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $(cssSelector("[data-test-id='date'] input")).setValue(planningDate);
+        $(cssSelector("[data-test-id='name'] input")).setValue("Иван Ромашкин");
+        $(cssSelector("[data-test-id='phone'] input")).setValue("+79270000000");
+        $(cssSelector("[data-test-id='agreement']")).click();
+        $(cssSelector("button.button")).click();
+        $(cssSelector("div.notification__content"))
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
+}
